afa9af0b997d597143f0b0875a7dd981562d080c
moook-git <moook.email@gmail.com>
2025-02-15 15:22:46 +0300

    Scritps fixes

--- scripts/observer.js
+++
@@ -1,8 +0,0 @@
-const observer = new IntersectionObserver( entries => {
-    entries.forEach( entry => {
-        entry.isIntersecting ? entry.target.classList.add('start') : entry.target.classList.remove('start')
-    })
-})
-
-var animateElements = document.querySelectorAll('.animate')
-animateElements.forEach( element => observer.observe(element))
